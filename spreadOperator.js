341d63a3ae7d212217f571da067beb0890c3b92d
regycloud <regy888@gmail.com>
2021-04-21 11:07:34 +0700

    Adding spreadOperator for array

---
+++ spreadOperator.js
@@ -0,0 +1,22 @@
+// Before using spread Operator
+/*
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+ 
+console.log(favorites);
+
+*/
+
+/* output
+[ 'Seafood', 'Salad', 'Nugget', 'Soup' ]
+*/
+
+
+// Using Spread Operator
+
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+ 
+console.log(...favorites);
+ 
+/* output
+[ 'Seafood', 'Salad', 'Nugget', 'Soup' ]
+*/
